6ce536b97c2fd31efbb1ed30b613c63cca6f9674
Aidi Stan <aidistan@live.com>
2014-11-25 16:44:30 +0800

    Fix a bug
    Close the menu when back to top.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@ $(function() {
   $( '#dl-menu' ).dlmenu({
     animationClasses : { classin : 'dl-animate-in', classout : 'dl-animate-out' }
   });
+  $( '.dl-close').on('click', function(){
+    $('#dl-menu').data('dlmenu').closeMenu();
+  });
 });
 
 // FitVids options
@@ -42,7 +45,7 @@ $(document).ready(function() {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
     removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
+    // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: 'mfp-fade'
   });
